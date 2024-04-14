3a6fc9c12cc2d13fd9463965c15b0bb129250c6d
Alin Triff <triffalin@gmail.com>
2024-04-14 19:18:33 +0300

    created components file

---
+++ src/components/BotManager.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BotManager = () => {
+  return (
+    <div>
+      <h1>Bot Manager</h1>
+    </div>
+  );
+};
+
+export default BotManager;
---
+++ src/components/TradeVisualizer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TradeVisualizer = () => {
+  return (
+    <div>
+      <h1>Trade Visualizer</h1>
+    </div>
+  );
+};
+
+export default TradeVisualizer;
