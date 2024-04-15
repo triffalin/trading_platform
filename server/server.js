ef799d71df5b6267aa0b6022a374e29d7fa91e67
Alin Triff <triffalin@gmail.com>
2024-04-15 14:55:34 +0300

    eliminated the warning for mongoose.connect

--- server/server.js
+++ server/server.js
@@ -10,10 +10,7 @@ const app = express();
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
