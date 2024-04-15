ffc17dd19ae64ea62ad6ff7a83c603caaef08ed3
Alin Triff <triffalin@gmail.com>
2024-04-15 14:49:52 +0300

    connected the mongodb

--- server/server.js
+++ server/server.js
@@ -12,8 +12,7 @@ const app = express();
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
   })
   .then(() => {
     console.log('Connected to MongoDB');
