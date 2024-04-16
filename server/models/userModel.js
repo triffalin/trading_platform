f7d81e94be1c74939b79e298a049bbc21c62feeb
Alin Triff <triffalin@gmail.com>
2024-04-16 21:10:50 +0300

    update the model to include fields for password reset tokens and expiration times

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,30 +2,12 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 // Define the schema for the user
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true
-    }
-    // Add any other user attributes you need, such as roles, profiles, etc.
-    // ...
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  passwordResetToken: String,
+  passwordResetExpires: Date
+});
 
 // Pre-save hook to hash password before saving a new user
 userSchema.pre('save', async function (next) {
