cea0df6aa9f1dc3f1dc0726ada2b242bd909fd22
Alin Triff <triffalin@gmail.com>
2024-04-26 16:53:08 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
+    // Add more paths here if you have other directories with Tailwind classes
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
