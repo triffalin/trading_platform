08fa012d7a7944b0fae643a66cdab961fca94d95
Alin Triff <triffalin@gmail.com>
2024-05-02 16:25:09 +0300

    fixed all the links from sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,18 +12,38 @@ const Sidebar = () => {
           <Link href="/dashboard">Dashboard</Link>
         </li>
         <li className={router.pathname === '/portfolio' ? 'active' : ''}>
-          <Link href="/portfolio">My Portfolio</Link>
+          <Link href="/accounts">My Portfolio</Link>
         </li>
         <li className={router.pathname === '/trading' ? 'active' : ''}>
-          <Link href="/trading">Trading Bot</Link>
+          <Link href="/control_panel">Control Panel</Link>
+        </li>
+        <li className={router.pathname === '/trading' ? 'active' : ''}>
+          <Link href="/signal_bots">Signal Bot</Link>
+        </li>
+        <li className={router.pathname === '/trading' ? 'active' : ''}>
+          <Link href="/bots">DCA Bot</Link>
+        </li>
+        <li className={router.pathname === '/trading' ? 'active' : ''}>
+          <Link href="/grid_bots">GRID Bot</Link>
+        </li>
+        <li className={router.pathname === '/trading' ? 'active' : ''}>
+          <Link href="/smart_trades/new">Smart Trade</Link>
         </li>
         <li className={router.pathname === '/terminal' ? 'active' : ''}>
-          <Link href="/terminal">Terminal</Link>
+          <Link href="/trades_terminal">Terminal</Link>
         </li>
         <li className={router.pathname === '/marketplace' ? 'active' : ''}>
           <Link href="/marketplace">Marketplace</Link>
         </li>
-        {/* Add more sections as necessary */}
+        <li className={router.pathname === '/marketplace' ? 'active' : ''}>
+          <Link href="/dashboard/apps">Store</Link>
+        </li>
+        <li className={router.pathname === '/marketplace' ? 'active' : ''}>
+          <Link href="/tracking_codes">Invite Friends</Link>
+        </li>
+        <li className={router.pathname === '/marketplace' ? 'active' : ''}>
+          <Link href="/users/subscriptions/pricing">Subscriptions</Link>
+        </li>
       </ul>
     </aside>
   );
