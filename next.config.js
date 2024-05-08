a986827ba5f416f982047e8460c2a1b5a61782bb
Alin Triff <triffalin@gmail.com>
2024-05-08 13:41:08 +0300

    fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/auth//login',
-        destination: '/api/auth//login',
+        source: '/auth/login',
+        destination: '/api/auth/login'
+      },
+      {
         source: '/auth/registration',
-        destination: '/api/auth/registration',
+        destination: '/api/auth/registration'
+      },
+      {
         source: '/auth/forgot-password',
         destination: '/api/auth/forgot-password'
       }
