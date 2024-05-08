a8f0e896bb10669dca39eb78d009fa3399994e93
Alin Triff <triffalin@gmail.com>
2024-05-08 18:50:01 +0300

    added an additional layer of reliability and customization to my application, making it more robust and user-friendly

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import '@/styles/globals.css';
+import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { ThemeProvider } from '@/contexts/ThemeContext';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <ErrorBoundary>
+          <Component {...pageProps} />
+        </ErrorBoundary>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
