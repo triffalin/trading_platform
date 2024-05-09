1b29c36c8099d9eeaadb972c74006cf34f25d219
Alin Triff <triffalin@gmail.com>
2024-05-09 13:48:33 +0300

    maked application more resilient and user-friendly in the face of unexpected errors

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,4 +1,6 @@
 import React, { Component, ErrorInfo, ReactNode } from 'react';
+import * as Sentry from '@sentry/react';
+import Link from 'next/link';
 
 interface Props {
   children: ReactNode;
@@ -14,18 +16,32 @@ export class ErrorBoundary extends Component<Props, State> {
   };
 
   public static getDerivedStateFromError(_: Error): State {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     console.error('Uncaught error:', error, errorInfo);
+    // Convert ErrorInfo into a plain object for Sentry
+    const errorDetails = {
+      componentStack: errorInfo.componentStack
+    };
+    Sentry.captureException(error, { extra: errorDetails });
   }
 
   public render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return (
+        <div>
+          <h1>Oops! Something went wrong.</h1>
+          <p>
+            We&apos;re having trouble displaying this part of our site.
+            We&apos;ve noted the issue and will look into it!
+          </p>
+          <Link href="/" passHref>
+            <a>Go to Home</a>
+          </Link>
+        </div>
+      );
     }
 
     return this.props.children;
