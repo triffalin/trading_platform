a74d6e73fc05702b78d52b7eb7a3a1608196948f
Alin Triff <triffalin@gmail.com>
2024-05-09 17:34:00 +0300

    improved the security, reliability, and maintainability of your email utility

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -9,12 +9,17 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendResetPasswordEmail = async (to, token) => {
-  const link = `http://localhost:3000/auth/forgot-password?token=${token}`;
-  await transporter.sendMail({
-    from: '"Qtrading" <${process.env.EMAIL_USERNAME}>',
-    to: to,
-    subject: 'Reset Your Password - Qtrading',
-    text: `To reset your password, please click on this link: ${link}`,
-    html: `<b>To reset your password, please click on this link:</b> <a href="${link}">${link}</a>` // HTML body
-  });
+  const link = `${process.env.BASE_URL}/auth/forgot-password?token=${token}`;
+  try {
+    await transporter.sendMail({
+      from: `"Qtrading" <${process.env.EMAIL_USERNAME}>`,
+      to: to,
+      subject: 'Reset Your Password - Qtrading',
+      text: `To reset your password, please click on this link: ${link}`,
+      html: `<b>To reset your password, please click on this link:</b> <a href="${link}">${link}</a>` // HTML body
+    });
+  } catch (error) {
+    console.error('Error sending reset password email:', error);
+    throw new Error('Failed to send reset password email.');
+  }
 };
