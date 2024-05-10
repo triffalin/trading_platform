6de24e9c33b320b1cb45b63a653aa32070784845
Alin Triff <triffalin@gmail.com>
2024-05-10 13:12:58 +0300

    Implemented crypto.randomBytes for secure token generation + Streamlined the method check and error responses to enhance clarity and security

--- src/pages/api/auth/forgot-password.ts
+++ src/pages/api/auth/forgot-password.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { sendResetPasswordEmail } from '@/lib/mailer.js';
+import { randomBytes } from 'crypto';
+import { sendResetPasswordEmail } from '@/lib/mailer';
 
 const prisma = new PrismaClient();
 
@@ -8,40 +9,38 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
-    const { email } = req.body;
-
-    try {
-      const user = await prisma.user.findUnique({
-        where: { email }
-      });
-
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-
-      const resetToken = Math.random().toString(36).substr(2);
-      const tokenExpiry = new Date(Date.now() + 1000 * 60 * 60); // 1 hour from now
-
-      await prisma.user.update({
-        where: { email },
-        data: {
-          resetToken,
-          resetTokenExpiry: tokenExpiry
-        }
-      });
-
-      await sendResetPasswordEmail(email, resetToken);
-
-      res
-        .status(200)
-        .json({ message: 'Reset password link has been sent to your email.' });
-    } catch (error) {
-      console.error('Forgot password error:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  } else {
+  if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  const { email } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const resetToken = randomBytes(32).toString('hex');
+    const tokenExpiry = new Date(Date.now() + 3600000); // 1 hour from now
+
+    await prisma.user.update({
+      where: { email },
+      data: {
+        resetToken,
+        resetTokenExpiry: tokenExpiry
+      }
+    });
+
+    await sendResetPasswordEmail(email, resetToken);
+
+    res
+      .status(200)
+      .json({ message: 'Reset password link has been sent to your email.' });
+  } catch (error: unknown) {
+    console.error('Forgot password error:', error);
+    const errorMessage = (error as Error).message || 'Unknown error';
+    res.status(500).json({ error: 'Internal server error', errorMessage });
   }
 }
