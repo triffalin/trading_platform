6f8ea7780831a83f514468a0a0121dd7b261aeb6
Alin Triff <triffalin@gmail.com>
2024-05-22 13:26:40 +0300

    Added comments to describe the purpose of the configuration and each plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+/**
+ * PostCSS configuration file
+ * This file contains the configuration settings for PostCSS,
+ * including the plugins used for CSS processing.
+ * @type {import('postcss').ProcessOptions}
+ */
+
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    autoprefixer: {}
+  }
+};
